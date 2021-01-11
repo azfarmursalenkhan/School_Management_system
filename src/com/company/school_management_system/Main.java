9c9ef0f28654444e719c21bd4dda1cecff24c530
azfarmursalenkhan <azfarmursalenkhan@yahoo.com>
2021-01-11 11:05:47 +0100

    School Finance management system

---
+++ src/com/company/school_management_system/Main.java
@@ -0,0 +1,11 @@
+package com.company.school_management_system;
+//This class is responsible for keep track of the students
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
---
+++ src/com/company/school_management_system/Student.java
@@ -0,0 +1,14 @@
+package com.company.school_management_system;
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade){
+
+    }
+
+}
